d34a50f0512d2a5d7ddb6c7df7cdb8f4e327ca82
Ale <alessandra.na001@gmail.com>
2021-05-08 00:03:10 -0500

    Hecha la vista del juego SOLO PARA GALAXY S5

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './styles.css';
-// import './assets/style.css';
+import './assets/style.css';
 import App from './App.js';
 import store from './store';
 import { State } from './models/state';
